58a900342e5f9acd6ed6df0e0611d9b4abaf34d2
galihandyan <andyangalih@gmail.com>
2021-03-31 09:32:49 +0700

    complete answer

--- replaceWord#2/index.js
+++ replaceWord#2/index.js
@@ -3,6 +3,13 @@ const idx = Math.floor(Math.random() * 4)
 
 const main = (data, message) => {
   // code here
+  let arrMessage = message.split(' ')
+  let idxName = arrMessage.indexOf('{name}')
+  let idxGPA = arrMessage.indexOf('{GPA}')
+
+  arrMessage.splice(idxName, 1, data.name)
+  arrMessage.splice(idxGPA, 1, data.GPA)
+  return arrMessage.join(' ')
 }
 
 // don't change code below
